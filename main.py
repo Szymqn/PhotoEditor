c65a7c70b3d1129ec1bbbd42925ee0d17c4bd283
szymqn <koodziejskisz@outlook.com>
2023-05-01 17:27:10 +0200

    add additive mix

--- main.py
+++ main.py
@@ -29,7 +29,9 @@ class App(QMainWindow):
         self.darkness_slider_l = None
         self.darkness_slider_p = None
         self.negative_checkbox = None
-        self.pixmap = None
+        self.additive_checkbox = None
+        self.pixmap1 = None
+        self.pixmap2 = None
 
         layout = QVBoxLayout()
 
@@ -73,10 +75,10 @@ class App(QMainWindow):
         imagePath = image[0]
         if imagePath:
             pixmap = QPixmap(imagePath)
-            self.pixmap = pixmap.scaled(self.photo_w, self.photo_h)
-            self.org_photo1.setPixmap(self.pixmap)
+            self.pixmap1 = pixmap.scaled(self.photo_w, self.photo_h)
+            self.org_photo1.setPixmap(self.pixmap1)
             self.org_photo1.adjustSize()
-            self.mod_photo.setPixmap(self.pixmap)
+            self.mod_photo.setPixmap(self.pixmap1)
             self.mod_photo.adjustSize()
             self.upload_button1.setVisible(False)
             self.photoLabels()
@@ -91,10 +93,12 @@ class App(QMainWindow):
         imagePath = image[0]
         if imagePath:
             pixmap = QPixmap(imagePath)
-            self.pixmap = pixmap.scaled(self.photo_w, self.photo_h)
-            self.org_photo2.setPixmap(self.pixmap)
+            self.pixmap2 = pixmap.scaled(self.photo_w, self.photo_h)
+            self.org_photo2.setPixmap(self.pixmap2)
             self.org_photo2.adjustSize()
             self.upload_button2.setVisible(False)
+            self.mixedLabels()
+            self.showMixed()
 
     def photoLabels(self):
         photo1_label = QLabel('Original Photo', self)
@@ -113,6 +117,11 @@ class App(QMainWindow):
         desc2_label = QLabel('Power methods', self)
         desc2_label.show()
         desc2_label.setGeometry(950, 475, 200, 30)
+
+    def mixedLabels(self):
+        mix_label = QLabel('Blending methods', self)
+        mix_label.show()
+        mix_label.setGeometry(1200, 20, 200, 20)
 
     def showBrightness(self):
         # liner
@@ -182,14 +191,20 @@ class App(QMainWindow):
         self.negative_checkbox.move(630, 750)
         self.negative_checkbox.show()
 
+    def showMixed(self):
+        self.additive_checkbox = QCheckBox('Additive', self)
+        self.additive_checkbox.toggled.connect(self.additive)
+        self.additive_checkbox.move(1200, 50)
+        self.additive_checkbox.show()
+
     def brightFactorL(self):
         factor = 1 + (self.sender().value() * 0.1)
-        new_pixmap = QPixmap(self.pixmap.size())
+        new_pixmap = QPixmap(self.pixmap1.size())
         painter = QPainter(new_pixmap)
 
         for i in range(self.photo_w):
             for j in range(self.photo_h):
-                c = self.pixmap.toImage().pixel(i, j)
+                c = self.pixmap1.toImage().pixel(i, j)
                 colors = QColor(c).getRgb()
                 new_colors = list(
                     (int(colors[0] * factor), int(colors[1] * factor), int(colors[2] * factor), colors[3]))
@@ -207,12 +222,12 @@ class App(QMainWindow):
     def brightFactorP(self):
         factor = (self.sender().value() / 10)
         n = 1.33
-        new_pixmap = QPixmap(self.pixmap.size())
+        new_pixmap = QPixmap(self.pixmap1.size())
         painter = QPainter(new_pixmap)
 
         for i in range(self.photo_w):
             for j in range(self.photo_h):
-                c = self.pixmap.toImage().pixel(i, j)
+                c = self.pixmap1.toImage().pixel(i, j)
                 colors = QColor(c).getRgb()
                 new_colors = list(
                     (int(factor * colors[0] ** n), int(factor * colors[1] ** n), int(factor * colors[2] ** n), colors[3]))
@@ -229,12 +244,12 @@ class App(QMainWindow):
 
     def darkFactorL(self):
         factor = 1 - (self.sender().value() * 0.1)
-        new_pixmap = QPixmap(self.pixmap.size())
+        new_pixmap = QPixmap(self.pixmap1.size())
         painter = QPainter(new_pixmap)
 
         for i in range(self.photo_w):
             for j in range(self.photo_h):
-                c = self.pixmap.toImage().pixel(i, j)
+                c = self.pixmap1.toImage().pixel(i, j)
                 colors = QColor(c).getRgb()
                 new_colors = (int(colors[0] * factor), int(colors[1] * factor), int(colors[2] * factor), colors[3])
                 painter.setPen(QColor(*new_colors))
@@ -246,12 +261,12 @@ class App(QMainWindow):
     def darkFactorP(self):
         factor = 1 - (self.sender().value() * 0.1)
         n = 0.8
-        new_pixmap = QPixmap(self.pixmap.size())
+        new_pixmap = QPixmap(self.pixmap1.size())
         painter = QPainter(new_pixmap)
 
         for i in range(self.photo_w):
             for j in range(self.photo_h):
-                c = self.pixmap.toImage().pixel(i, j)
+                c = self.pixmap1.toImage().pixel(i, j)
                 colors = QColor(c).getRgb()
                 new_colors = (int(factor * colors[0] ** n), int(factor * colors[1] ** n), int(factor * colors[2] ** n), colors[3])
                 painter.setPen(QColor(*new_colors))
@@ -261,18 +276,38 @@ class App(QMainWindow):
         self.mod_photo.setPixmap(new_pixmap)
 
     def negative(self):
-
-        new_pixmap = QPixmap(self.pixmap.size())
+        new_pixmap = QPixmap(self.pixmap1.size())
         painter = QPainter(new_pixmap)
 
         for i in range(self.photo_w):
             for j in range(self.photo_h):
-                c = self.pixmap.toImage().pixel(i, j)
+                c = self.pixmap1.toImage().pixel(i, j)
                 colors = QColor(c).getRgb()
                 if self.sender().isChecked():
                     new_colors = (int(255 - colors[0]), int(255 - colors[1]), int(255 - colors[2]), colors[3])
                 else:
                     new_colors = (int(colors[0]), int(colors[1]), int(colors[2]), colors[3])
+                painter.setPen(QColor(*new_colors))
+                painter.drawPoint(i, j)
+
+        painter.end()
+        self.mod_photo.setPixmap(new_pixmap)
+
+    def additive(self):
+        new_pixmap = QPixmap(self.pixmap1.size())
+        painter = QPainter(new_pixmap)
+        print('additive')
+
+        for i in range(self.photo_w):
+            for j in range(self.photo_h):
+                c1 = self.pixmap1.toImage().pixel(i, j)
+                colors1 = QColor(c1).getRgb()
+                c2 = self.pixmap2.toImage().pixel(i, j)
+                colors2 = QColor(c2).getRgb()
+                if self.sender().isChecked():
+                    new_colors = (int(min(colors1[0] + colors2[0], 255)), int(min(colors1[1] + colors2[1], 255)), int(min(colors1[2] + colors2[2], 255)), colors1[3])
+                else:
+                    new_colors = (int(colors1[0]), int(colors1[1]), int(colors1[2]), colors1[3])
                 painter.setPen(QColor(*new_colors))
                 painter.drawPoint(i, j)
 
